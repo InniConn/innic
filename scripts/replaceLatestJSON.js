b45dd96afbb786d0c5475c4c27939d5619a19cd4
Robot <43430022+RobotVol@users.noreply.github.com>
2024-06-12 16:08:35 +0000

    feat: replace url

---
+++ scripts/replaceLatestJSON.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const latestFile = path.join(__dirname, '../latest.json');
+
+
+fs.readFile(latestFile, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    data = data.replace(/https:\/\/github.com\/InniConn\/innic\/releases\/download\/\d+\.\d+\.\d+\//g, 'https://inniconn.innic.app/client_latest/');
+    fs.writeFile(latestFile, data, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('latest.json updated');
+    });
+});
