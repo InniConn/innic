4d24b94d17ab1ce67bfaf23878dd1b569e5815d8
Robot <43430022+RobotVol@users.noreply.github.com>
2024-06-13 18:05:40 +0800

    chore: verison

--- scripts/replaceLatestJSON.js
+++ scripts/replaceLatestJSON.js
@@ -3,14 +3,16 @@ const path = require('path');
 
 const latestFile = path.join(__dirname, '../latest/latest.json');
 
+const version = process.env.VERSION
 
+console.log('verison',version)
 fs.readFile(latestFile, 'utf8', (err, data) => {
     if (err) {
         console.error(err);
         return;
     }
 
-    data = data.replace(/https:\/\/github.com\/InniConn\/innic\/releases\/download\/\d+\.\d+\.\d+\//g, 'https://inniconn.innic.app/client_latest/');
+    data = data.replace(/https:\/\/github.com\/InniConn\/innic\/releases\/download\/\d+\.\d+\.\d+\//g, `https://inniconn.innic.app/${version}/`);
     fs.writeFile(latestFile, data, (err) => {
         if (err) {
             console.error(err);
